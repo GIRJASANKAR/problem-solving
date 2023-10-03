3462d4097bb6deac425533d15a9573760bf6f373
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-10-03 16:22:33 +0530

    created a node

---
+++ created_node.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class LinkList{
+    public:
+     int myval;
+     LinkList* nextValAddress;
+};
+
+
+
+
+int main() {
+    LinkList* N1=new LinkList();
+    cout<<N1->myval<<endl;
+	cout<<"GfGhjjj!";
+	return 0;
+}
