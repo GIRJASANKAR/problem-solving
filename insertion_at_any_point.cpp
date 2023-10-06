007d4ed28f423ddaf581e00c8ebd3faa169ecaa6
Girja Sankar Tiwari <63961074+GIRJASANKAR@users.noreply.github.com>
2023-10-06 17:22:44 +0530

    insertion_at_any_point

---
+++ insertion_at_any_point.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+     int data;
+     Node* next;
+  Node(int x){
+     this->data=x;
+     this->next=NULL;
+ }
+};
+
+void insertAtHead(Node* &head,Node* node2){
+    node2->next=head;
+    head=node2;
+}
+
+
+void printLL(Node* head){
+      Node* ptr=head;
+    while(ptr!=NULL){
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+}
+
+void insertAtTail(Node* &tail,Node* node5){
+      tail->next=node5;
+      tail=node5;
+}
+
+void insertATPosition(Node* & head,int pos, Node* mynode){
+    Node* ptr=head; // 25, 15,150.   pos=2
+    if(pos==1){
+        insertAtHead(head,mynode);
+        return;
+    }
+    
+    while(pos!=1){
+        ptr=ptr->next;
+        pos--;
+    }
+    
+    
+    if(ptr->next==NULL){
+       insertAtTail(ptr,mynode);
+       return;
+    }
+    
+    Node* temp=ptr->next;
+    ptr->next=mynode;
+    mynode->next=temp;
+}
+
+int main(){
+     Node* node1=new Node(15);
+     Node* head=node1;
+     Node* tail=node1;
+     Node* node2= new Node(25);
+     Node* node4=new Node(150);
+     insertAtHead(head,node2);
+     insertAtTail(tail,node4);
+     Node* node5=new Node(30); // 25,15,30,150;
+       Node* node6=new Node(5);
+             Node* node7=new Node(160);
+     insertATPosition(head,2,node5);
+      insertATPosition(head,2,node6);
+ insertATPosition(head,5,node7);
+  Node* node8=new Node(1);
+ insertATPosition(head,1,node8);
+   
+     printLL(head);
+    return 0;
+}
+
